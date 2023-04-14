1363a1ee1325e22e913b598943b57fef22744727
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-04-14 11:12:40 -0300

    iniciar proyecto react

---
+++ src/components/UnComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UnComponent = () => {
+  return (
+    <>
+        <h2>Soy un componente</h2>
+        <p>Estoy envuelto en un fragment</p>
+    </>
+  )
+}
+
+export default UnComponent
